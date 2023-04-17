cb86396a9a988d3fbdd925ed86151d7b1153394d
zhuminghang <a3244005396ABCF>
2023-04-17 19:30:53 +0800

    修改文件

---
+++ OnlineSupermarketTuto/Models/Functions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OnlineSupermarketTuto.Models
+{
+
+    public class Functions
+    {
+        public SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+
+        public Functions()
+        {
+            ConStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Administrator\\Documents\\SupermarketDb.mdf;Integrated Security=True;Connect Timeout=30";
+            Con=new SqlConnection(ConStr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="Query"></param>
+        /// <returns></returns>
+        public DataTable GetData(string Query)
+        {
+            DataTable dt = new DataTable();
+            sda = new SqlDataAdapter(Query, ConStr);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="Query"></param>
+        /// <returns></returns>
+        public int SetData(string Query)
+        {
+            int cnt = 0;
+            if(Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            Cmd.CommandText = Query;
+            cnt = Cmd.ExecuteNonQuery();
+            Con.Close();
+            return cnt;
+        }
+    }
+}
